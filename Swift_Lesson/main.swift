8d0b9181eb68d8c2910c39c133c434af6e470b41
mansur.sarxanov <sarxanov.mansur@gmail.com>
2025-02-19 10:40:25 +0400

    absctraction

--- Swift_Lesson/main.swift
+++ Swift_Lesson/main.swift
@@ -7,19 +7,68 @@
 
 import Foundation
 
-var counts = [1,2,3,4,5,6,7,8,9,10]
-var countList1 = [Int]()
-var countList2 = [Int]()
+//var counts = [1,2,3,4,5,6,7,8,9,10]
+//var countList1 = [Int]()
+//var countList2 = [Int]()
+//  
+//  
+//for i in counts
+//{
+//    if i%2==0
+//    { 
+//        countList1.append(i)
+//    }
+//    else
+//    {
+//        countList2.append(i)
+//    }
+//}
+//print(countList1)
+//print(countList2)
+// 
 
 
-for i in counts
-{
-    if i%2==0
-    {
-        countList1.append(i)
-    }else{
-        countList2.append(i)
+class Vehicles{
+    var wheelCount : Int!
+    var customerCapacity : Int
+    init(customerCapacity: Int) {
+        self.customerCapacity = customerCapacity
     }
+
+    func goFront(){}
+    func goBack(){}
 }
-print(countList1)
-print(countList2)
+
+class BigVehicles : Vehicles
+{
+    let speedLimit = 88
+   
+}
+
+
+
+class Car : Vehicles
+{
+     init() {
+        super.init(customerCapacity: 4)
+    }
+    
+}
+
+class Truck : BigVehicles
+{
+    init() {
+       super.init(customerCapacity: 2)
+   }
+   
+    
+}
+
+class Bus : BigVehicles
+{
+    init() {
+       super.init(customerCapacity: 55)
+   }
+   
+    
+}
